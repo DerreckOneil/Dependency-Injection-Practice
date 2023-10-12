2dd28d570745d65f23dafa3fea264a21cfa75458
Carlos DaLomba <carlos@2kpixelstudios.net>
2023-10-12 01:38:53 -0400

    Added DI example with KrakenIoc

---
+++ BasicExample/Implementations/DependentClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BasicExample {
+    public class DependentClass : IDisposable {
+        private IHttpServer server;
+
+        public DependentClass(IHttpServer server) {
+            this.server = server;
+
+            Console.WriteLine(nameof(DependentClass) + "'s ctor");
+            if (server != null)
+                server.Listen(65535);
+        }
+
+        public void Dispose() {
+            server.Stop();
+            Console.WriteLine("Dispose()");
+        }
+    }
+}
--- BasicExample/Program.cs
+++ BasicExample/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using AOFL.KrakenIoc.Core.V1;
 using AOFL.KrakenIoc.Core.V1.Interfaces;
 
@@ -24,12 +25,14 @@ namespace BasicExample {
     }
 
     public class Program {
-        public static void Main() {
+        private static IContainer container;
+
+        public static async Task Main() {
             Console.WriteLine("Hello world");
 
-            IContainer container = new Container();
-
+            container = new Container();
             //LINQ Example
+            //TODO: Split this out into a different little C# program
             List<Transform> transforms = new List<Transform> {
                 new Transform { localPosition = new Vector3(1, 2, 3) },
                 new Transform { localPosition = new Vector3(1, 7, 3) },
@@ -39,8 +42,19 @@ namespace BasicExample {
             foreach (Vector3 vec in transforms.Select(t => t.localPosition).Where(l => l.y > 5))
                 Console.WriteLine(vec);
 
-            //container.Bind<IBattery, AAABattery>();
             container.Bind<IHttpServer, MockHttpServer>();
+            container.Bind<DependentClass>();
+
+            //MAYBE you need to "build" the container here, depending on the DI framework
+
+            DependentClass c = container.Resolve<DependentClass>();
+
+            Console.WriteLine("Waiting 2sec...");
+            await Task.Delay(2000);
+            Console.WriteLine("Returning back to the main program!");
+
+            //WARNING: This does NOT call DependentClass's Dispose method!
+            container.Dispose();
         }
     }
 }
