083ab4f80800dfe43e6ad90e6bf3c8a48b841f20
Derreck Oneil <derreckoneil@gmail.com>
2023-10-11 23:50:00 -0400

    Initial Commit

---
+++ DependencyInjectionPractice/BasicExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
